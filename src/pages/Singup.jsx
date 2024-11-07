76fb1082d617c3113da7c9e8dd7cbe3979708535
ferin <sudawan91@gmail.com>
2024-11-07 11:04:58 +0700

    first

---
+++ src/pages/Singup.jsx
@@ -0,0 +1,129 @@
+<Box className=" h-[90rem] w-[144rem] relative bg-[#ffffff]">
+  <Box className=" h-[45.000003051757815rem] w-[144rem] absolute left-[-0.0000030517578125rem] top-0 bg-[] undefined undefined"></Box>
+  <Box className=" h-48 w-[13.6rem]">
+    <Box className=" h-[4.5rem] w-[13.3rem] bg-[#212121] absolute left-[124.39998779296874rem] top-34"></Box>
+    <Typography className=" text-right align-top absolute left-[128.39998779296874rem] top-[4.700048828125rem] w-max w-max">
+      SIGN IN
+    </Typography>
+  </Box>
+  <Box className="  rounded-[3.5rem] h-[58.6rem] w-[57rem] bg-[#ffffff]"></Box>
+  <Stack
+    direction="col"
+    alignItems="start"
+    gapY={16}
+    className="h-max absolute left-[52.8rem] top-[25.775rem] bg-[#ffffff]"
+  >
+    <Box className=" h-[20.45rem] w-[38.0000244140625rem]">
+      <Box className=" h-[6.8rem] w-[38rem] absolute left-0 top-[6.85rem]">
+        <Box className=" h-[6.6rem] w-[37.8rem] bg-[#ffffff] absolute left-0 top-[6.85rem]"></Box>
+        <Box className=" h-[4.972917175292968rem] w-[33.023809814453124rem] absolute left-16 top-80">
+          <Box className=" h-[4.972917175292968rem] w-[33.023809814453124rem] absolute left-16 top-80">
+            <Typography className=" text-left align-middle absolute left-16 top-[9.895849609375rem] w-max">
+              johndoe@example.com
+            </Typography>
+            <Typography className=" text-left align-top absolute left-16 top-80 w-max w-max">
+              EMAIL ADRRESS
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+      <Box className=" h-[6.8rem] w-[38rem] absolute left-0 top-[13.65rem]">
+        <Box className=" h-[6.6rem] w-[37.8rem] bg-[#ffffff] absolute left-0 top-[13.65rem]"></Box>
+        <Box className=" h-16 w-16 relative">
+          <Box className=" h-16 w-16 absolute left-0 top-0">
+            <Box className=" h-16 w-16 bg-[#000000] absolute left-0 top-0"></Box>
+            <Box className="">
+              <Box className=" h-[1.1000000953674316rem] w-16 absolute left-0 top-2 undefined undefined"></Box>
+              <Box className=" h-[0.9000000953674316rem] w-[1.3656005859375rem] absolute left-1 top-[0.35000001192092894rem] undefined undefined"></Box>
+            </Box>
+            <Box className="">
+              <Box className=" h-[0.5rem] w-4 absolute left-[0.55rem] top-[0.55rem] undefined undefined"></Box>
+              <Box className=" h-6 w-[0.7rem] absolute left-4 top-4 undefined undefined"></Box>
+            </Box>
+          </Box>
+        </Box>
+        <Box className=" h-46 w-[33.023809814453124rem] absolute left-16 top-[14.839599609375rem]">
+          <Box className=" h-46 w-[33.023809814453124rem] absolute left-16 top-[14.839599609375rem]">
+            <Typography className=" text-left align-middle absolute left-16 top-[16.695849609375rem] w-max">
+              **********
+            </Typography>
+            <Typography className=" text-left align-top absolute left-16 top-[14.839599609375rem] w-max w-max">
+              PASSWORD
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+      <Box className=" h-[6.8rem] w-[38rem] absolute left-0 top-0">
+        <Box className=" h-[6.6rem] w-[37.8rem] bg-[#ffffff] absolute left-0 top-0"></Box>
+        <Box className=" h-[4.972917175292968rem] w-[33.169976806640626rem] absolute left-[1.530029296875rem] top-[1.189599609375rem]">
+          <Typography className=" text-left align-middle absolute left-16 top-30 w-max">
+            John
+          </Typography>
+          <Typography className=" text-left align-top absolute left-[1.530029296875rem] top-[1.189599609375rem] w-max w-max">
+            FIRST NAME
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+    <Stack alignItems="start" gapX={8} className="w-max relative bg-[#ffffff]">
+      <Box className=" h-[2.199997329711914rem] w-[2.152433967590332rem]">
+        <Box className=" h-[1.999997329711914rem] w-[1.952433967590332rem] absolute left-0 top-0"></Box>
+        <Box className=" h-[0.7636354446411133rem] w-[1.1740547180175782rem] absolute left-4 top-[0.7181640625rem] undefined undefined"></Box>
+      </Box>
+      <Typography className=" text-left align-top underline w-max">
+        I agree to the Terms of Service and Privacy Policy.
+      </Typography>
+    </Stack>
+    <Box className=" h-64 w-[38rem]">
+      <Box className=" h-64 w-[38rem] bg-[#212121] absolute left-0 top-[25.95rem]"></Box>
+      <Typography className=" text-center align-top absolute left-[10.9rem] top-[27.75rem] w-max w-max">
+        CREATE AN ACCOUNT
+      </Typography>
+    </Box>
+  </Stack>
+  <Typography className=" text-center align-top absolute left-[53.000006103515624rem] top-[16.45rem] w-max h-max">
+    Sign up to Milky Tea-rex
+  </Typography>
+  <Typography className=" text-left align-top absolute left-[58.65000610351562rem] top-[20.6rem] w-max w-max">
+    Quick & Simple way to Automate your payment
+  </Typography>
+  <Typography className=" text-right align-top absolute left-[71.00003051757812rem] top-[60rem] w-max w-max">
+    OR
+  </Typography>
+  <Box className=" h-40 w-[17rem]">
+    <Box className=" h-40 w-40 absolute left-[63.400030517578124rem] top-[63.95rem]">
+      <Box className=" h-38 w-38 bg-[#fafafa] absolute left-[63.400030517578124rem] top-[63.95rem]"></Box>
+      <Box className=" h-28 w-28 absolute left-[64.00003051757812rem] top-[64.55rem]">
+        <Box className=" h-28 w-28 bg-[#000000] absolute left-[64.00003051757812rem] top-[64.55rem]"></Box>
+        <Box className=" h-[2.333333396911621rem] w-[2.333333396911621rem] absolute left-[64.23336181640624rem] top-[64.78330078125rem] bg-[#ffc107] undefined undefined"></Box>
+        <Box className=" h-[0.9047500610351562rem] w-18 absolute left-[64.36787719726563rem] top-[64.78330078125rem] bg-[#ff3d00] undefined undefined"></Box>
+        <Box className=" h-[0.9301834106445312rem] w-18 absolute left-[64.36140747070313rem] top-[66.186474609375rem] bg-[#4caf50] undefined undefined"></Box>
+        <Box className=" h-10 w-[1.1666667938232422rem] absolute left-[65.40003662109375rem] top-[65.71669921875rem] bg-[#1976d2] undefined undefined"></Box>
+      </Box>
+    </Box>
+    <Box className=" h-40 w-40 absolute left-[69.90003051757813rem] top-[63.95rem]">
+      <Box className=" h-38 w-38 bg-[#fafafa] absolute left-[69.90003051757813rem] top-[63.95rem]"></Box>
+      <Box className=" h-32 w-32 absolute left-[70.30003051757812rem] top-[64.35rem]">
+        <Box className=" h-32 w-32 bg-[#000000] absolute left-[70.30003051757812rem] top-[64.35rem]"></Box>
+        <Box className=" h-28 w-[2.1rem] absolute left-[70.80003051757812rem] top-[64.55rem]">
+          <Box className=" h-28 w-[2.1rem] bg-[#000000] absolute left-[70.80003051757812rem] top-[64.55rem]"></Box>
+          <Box className="">
+            <Box className=" h-18 w-20 absolute left-[70.82190551757813rem] top-[65.310546875rem] undefined undefined"></Box>
+            <Box className=" h-[0.585136890411377rem] w-[0.5153399467468261rem] absolute left-[71.85170288085938rem] top-[64.725rem] undefined undefined"></Box>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+    <Box className=" h-40 w-40 absolute left-[76.40003051757813rem] top-[63.95rem]">
+      <Box className=" h-38 w-38 bg-[#fafafa] absolute left-[76.40003051757813rem] top-[63.95rem]"></Box>
+      <Box className=" h-32 w-32 absolute left-[76.80003051757812rem] top-[64.35rem]">
+        <Box className=" h-32 w-32 bg-[#000000] absolute left-[76.80003051757812rem] top-[64.35rem]"></Box>
+        <Box className=" h-[2.533333396911621rem] w-[2.5333303451538085rem] absolute left-[77.13336791992188rem] top-[64.683349609375rem] bg-[#039be5] undefined undefined"></Box>
+        <Box className=" h-20 w-10 absolute left-[77.89716186523438rem] top-[65.203369140625rem] bg-[#ffffff] undefined undefined"></Box>
+      </Box>
+    </Box>
+  </Box>
+  <Typography className=" text-right align-top absolute left-[61.500006103515624rem] top-[84rem] w-max w-max">
+    © 2021. - 2025 All Rights Reserved. Qpay
+  </Typography>
+</Box>;
